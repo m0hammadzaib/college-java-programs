6c8073f3c0eee81305237a45182120cded93ff95
m0hammadzaib <zaibfr4@gmail.com>
2025-10-12 19:35:50 +0530

    Switch and Loops

--- Basics/conditionals.java
+++ Basics/conditionals.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class conditionals {
     public static void main(String[] args) {
         boolean sunrise = true;
@@ -11,5 +13,16 @@ public class conditionals {
         // &&
         // ||
         // !
+
+        Scanner one = new Scanner(System.in);
+        System.out.println("How much money do you have ?");
+
+        int cash = one.nextInt();
+
+        if (cash<50) {
+            System.out.println("Hatt gareeb");
+        }else{
+            System.out.println("How can i help you sir");
+        }
     }
 }
