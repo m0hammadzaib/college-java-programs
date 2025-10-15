3b7c27067e52dc5db24631c4c3f80b301e18d7ed
m0hammadzaib <zaibfr4@gmail.com>
2025-10-15 22:38:06 +0530

    Today's Code

---
+++ Basics/brkcont.java
@@ -0,0 +1,17 @@
+public class brkcont {
+    public static void main(String[] args) {
+        // break
+        int i = 0;
+        while (true) {
+            if (i==3) {
+                i = i+1;
+                continue;
+            }
+            System.out.println(i);
+            i = i+1;
+            if (i>5)
+                break;
+            
+        }
+    }
+}
---
+++ ExceptionHanding/errorhandling.java
@@ -0,0 +1,7 @@
+public class errorhandling{
+    public static void main(String[]args){
+        int[] marks = {24,87,91};
+
+        System.out.println(marks[3]);
+    }
+}
