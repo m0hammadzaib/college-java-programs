32a66cf57346d1f55d81f67e580df010657d41bc
m0hammadzaib <zaibfr4@gmail.com>
2025-10-16 16:22:54 +0530

    Try Catch Error

--- ExceptionHanding/errorhandling.java
+++ ExceptionHanding/errorhandling.java
@@ -2,6 +2,10 @@ public class errorhandling{
     public static void main(String[]args){
         int[] marks = {24,87,91};
 
-        System.out.println(marks[3]);
+        try{
+            System.out.println(marks[5]);
+        }catch(Exception exception){
+            System.out.println("Galat hai bhai");
+        }
     }
 }
