47996d3522981ee033a7434b43c799f9c27ae1d1
m0hammadzaib <zaibfr4@gmail.com>
2025-10-17 19:48:03 +0530

    Today's code

---
+++ Functions/func.java
@@ -0,0 +1,16 @@
+public class func{
+    public static void printhello(String name){
+        System.out.println("Hello "+name+" bhai");
+
+    }
+
+    public static void printSum(int a,int b){
+         int sum = a+b;
+         System.out.println(sum);
+      }
+    public static void main(String[]args){
+       printhello("Zaib");
+
+       printSum(10,77 );
+    }
+}
