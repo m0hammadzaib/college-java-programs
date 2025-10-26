ab196e395650a77aa4d2709efb4d7fe27a2fa88c
m0hammadzaib <zaibfr4@gmail.com>
2025-10-26 22:58:54 +0530

    Abstraction

--- OOPS/abstraction.java
+++ OOPS/abstraction.java
@@ -5,7 +5,15 @@ class animal{
 }
 
 class horse extends animal{
-    
+    public void walk(){
+        System.out.println("Walks on 4 legs");
+    }
+}
+
+class chicken extends animal{
+    public void walk(){
+        System.out.println("Walk on 2 legs");
+    }s
 }
 
 public class abstraction {
