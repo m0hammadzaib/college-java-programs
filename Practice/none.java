7359b6ef662333872d7b77f5d72befbebae49cca
m0hammadzaib <zaibfr4@gmail.com>
2025-11-02 00:52:00 +0530

    practice

--- Practice/none.java
+++ Practice/none.java
@@ -1,6 +1,9 @@
-
-public class none {
+public class none{
     public static void main(String[] args) {
-        
+        for (int i = 100; i <=500; i++) {
+            if(i%3==0 && i%9==0 && i%6==0){
+                 System.out.println(i);
+            }
+        }
     }
 }
